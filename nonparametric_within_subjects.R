fa3c13879ddcba0c4042d50ab694d8d7aff0519c
Peter Beshai <peter.beshai@gmail.com>
2014-06-16 12:34:05 -0700

    Add in brief report example

--- nonparametric_within_subjects.R
+++ nonparametric_within_subjects.R
@@ -5,7 +5,7 @@ all_data <- read.csv("sample_data.csv")
 columns <- c("likertA1","likertA2","likertA3")
 
 util.printBigHeader("Running Non-parametric Within-subjects Analysis")
-userDifficulty_results <- util.withinSubjectsNonParametricAnalysis(all_data, columns, dvName="rating")
+likert_results <- util.withinSubjectsNonParametricAnalysis(all_data, columns, dvName="rating")
 
 
 # Note that running this code will often produce warnings from R about ties and
